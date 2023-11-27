2d00b15026d8e32a7fe2ca9c44947f306e775d98
Annie <colberta31@gmail.com>
2023-11-27 16:05:13 +0000

    Time and weather data
    finished time and weather data

--- src/index.js
+++ src/index.js
@@ -2,8 +2,39 @@ function updateWeather(response) {
   let todayTemperatureElement = document.querySelector("#today-temperature");
   let todayTemp = response.data.temperature.current;
   let city = document.querySelector("#city-name");
+  let todayDescription = document.querySelector("#today-description");
+  let humidityToday = document.querySelector("#humidity-today");
+  let todayWindSpeed = document.querySelector("#wind-speed");
+  let todayTimeElement = document.querySelector("#today-time");
+  let date = new Date(response.data.time * 1000);
+
+  todayTimeElement.innerHTML = formatDay(date);
   city.innerHTML = response.data.city;
   todayTemperatureElement.innerHTML = Math.round(todayTemp);
+  todayDescription.innerHTML = response.data.condition.description;
+  humidityToday.innerHTML = `${response.data.temperature.humidity}%`;
+  todayWindSpeed.innerHTML = `${response.data.wind.speed} km/h`;
+}
+
+function formatDay(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
